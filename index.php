3f864c7321583fa50c2bba26a5c3316d3b24e6e2
Triv-johan <guyon.johan@laposte.net>
2020-10-27 13:48:31 +0100

    ejud

--- index.php
+++ index.php
@@ -1,11 +1,98 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+            integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+            crossorigin="anonymous">
+        <link href="style.css" rel="stylesheet">
+        <title>hertz</title>
+    </head>
+    <body>
+        <section>
+        <header><div class="headerdiv">
+            <div class="logonavbar"></div>
+            <nav class="navbar navbar-expand-sm bg-white navbar-white">
+                <a class="navbar-brand" href="#">
+                    <img
+                        src="image/langfr-1920px-Hertz-Logo.svg.png"
+                        alt="Logo"
+                        style="width:200px;">
+                </a>
+                <ul>
+                    <a href="#" style="color:black;">Liste véhicules</a>
+                </ul>
+                <ul>
+                    <a href="#" class="numerotel" style="color: black;">0606060606</a>
+                </ul>
+            </nav>
+</div>
+            </header>
+        
+        
+                <div class="présentation">
+                    <div class="titreaccueil"><h1>Bienvenue</h1></div>
+                    <div class="imgaccueil"><img src="image/hertz-choix-1024x640.jpg" style="justify-content: center;" class="img-fluid" alt="Responsive image"></div>
+                </div>
+            </section>
+                <section>
+            <div class="titretables">
+                <h2>Liste véhicule</h2>
+            </div>
+            <div class="liste">
+                <table class="table">
+                    <thead class="thead">
+                        <tr class="headtables">
+                            <th>Marque</th>
+                            <th>Modèle</th>
+                            <th>Année</th>
+                            <th>kilométrages</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Toyota</td>
+                            <td>Célica</td>
+                            <td>26/10/2020</td>
+                            <td>80 000</td>
+                        </tr>
+                        <tr>
+                            <td>Toyota</td>
+                            <td>Célica</td>
+                            <td>26/10/2020</td>
+                            <td>80 000</td>
+                        </tr>
+                        <tr>
+                            <td>Toyota</td>
+                            <td>Célica</td>
+                            <td>26/10/2020</td>
+                            <td>80 000</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+        
+        <section>
+            
+                <footer>
+                    <div class="container-fluid">
+                    <div class="row">
+                    <div class="info col-12 col-md-6">
+                        <iframe class="maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2737.8863249110263!2d5.551161015992889!3d46.668505379133705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478cd78d87b0768b%3A0x6dc52ab5581eb62b!2s2%20Route%20de%20Montaigu%2C%2039000%20Lons-le-Saunier!5e0!3m2!1sfr!2sfr!4v1603722796036!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    </div>
+                    <div class="maptitle col-12 col-md-6">
+                        <p>Lorem ipsum dolor<br> sit amet consectetur, adipisicing elit. A minus maxime alias incidunt suscipit aspernatur expedita nisi tempora autem assumenda. Lorem ipsum, dolor sit amet consectetur<br> adipisicing elit. Cum cumque ipsam necessitatibus quam, illum unde a?<br> Veritatis sapiente saepe voluptatem.</p>
+                    </div>
+                    
+                    </div>
+                    </div>
+                   
+            </footer>
+        </section>
+
+    </body>
 </html>
