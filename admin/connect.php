d6c61083e7c3859bac20062e588ceecc1548a6fa
loicdauchy <l.dauchy@codeur.online>
2020-10-29 14:02:38 +0100

    deleting useless thing

--- admin/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-  try {
-    $db = new PDO('mysql:host=localhost;dbname=hertz;port=3306', 'root', '');
-    }
-    catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
-}
-?>
