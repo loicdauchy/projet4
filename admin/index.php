7b86af805b07ad3a63c36de662fa230bd460f23c
loicdauchy <l.dauchy@codeur.online>
2020-10-29 15:51:06 +0100

    before mergin

--- admin/index.php
+++ admin/index.php
@@ -280,6 +280,7 @@ AFFICHER LES VOITURES -->
                     <input type='text' name='ville' value='".$client['ville_clients']."'>
                     <input type='text' name='mail' value='".$client['mail_clients']."'>
                     
+                    
                     <button type='submit' value='modifier' name='action'>Modifier</button>
                     <button type='submit' value='supprimer' name='action'>Supprimer</button>
                     
