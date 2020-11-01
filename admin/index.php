49bab474427aedca918dba8ff0a6513c1f135d56
loicdauchy <l.dauchy@codeur.online>
2020-11-01 16:09:31 +0100

    modif client

--- admin/index.php
+++ admin/index.php
@@ -178,12 +178,14 @@ AFFICHER LES VOITURES -->
                         $date = date("Y-m-d");
                         $recup1 = $db->query('SELECT * FROM louer WHERE id_client_clients ='.$client['id_client_clients'].'');
                         $recup1 = $recup1->fetch();
-                        if ($date > $recup1['date_fin']){
+                        if ($date >= $recup1['date_fin']){
                             echo "<input style='background:red; border-radius: 42px 42px 42px 42px;' type='text' name='late' value='En retard'>";
                         }else if($date >= $recup1['date_louer'] AND $date <= $recup1['date_fin']){
                             echo "<input style='background:green; border-radius: 42px 42px 42px 42px;' type='text' name='loc' value='En cours'>";
-                        }else {
-                            echo "<input style='background:blue; border-radius: 42px 42px 42px 42px;' type='text' name='pasLoc' value='Pas de location'>";
+                        }else if ($date < $recup1['date_louer'] AND $date < $recup1['date_fin']){
+                            echo "<input style='background:blue; border-radius: 42px 42px 42px 42px;' type='text' name='pasLoc' value='prévu prochainement'>";
+                        }else{
+                            echo "<input style='background:yellow; border-radius: 42px 42px 42px 42px;' type='text' name='pasLoc' value='Pas de location'>";
                         }
 ?>
 <?php                    
