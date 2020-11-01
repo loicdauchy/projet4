9219a1a4e261168494a32fcbac49ff80b59ebacc
loicdauchy <l.dauchy@codeur.online>
2020-11-01 16:00:20 +0100

    adding function

--- admin/index.php
+++ admin/index.php
@@ -62,197 +62,47 @@ include '../connect.php';
 ?>
 
 
-
-
-
 <?php
-
+include '../function.php';
 // AJOUTER UNE LOCATION
-    if(isset($_GET['action']) && $_GET['action']=="ajouter" && !empty($_GET['id'])  && !empty($_GET['idClient']) && !empty($_GET['dateloc']) && !empty($_GET['datefinloc'])){
-        $ajouter = $db->prepare('INSERT INTO louer (id_car_vehicules, id_client_clients, date_louer, date_fin) VALUES (:idCar, :idClient, :dateloc, :datefinloc)');
-        $ajouter->bindParam(':idCar', $_GET['id'], 
-        PDO::PARAM_STR);
-        $ajouter->bindParam(':idClient', $_GET['idClient'], 
-        PDO::PARAM_STR);
-        $ajouter->bindParam(':dateloc', $_GET['dateloc'], 
-        PDO::PARAM_STR);
-        $ajouter->bindParam(':datefinloc', $_GET['datefinloc'], 
-        PDO::PARAM_STR);
-        $estceok = $ajouter->execute();
-        // $estceok->debugDumpParams();
-            if($estceok){
-                echo 'votre enregistrement a été ajouté avec succés';
-                
-            
-            } else {
-                echo 'Veuillez recommencer svp, une erreur est survenue';
-            }
+    if(isset($_GET['action']) && $_GET['action']=="ajouter" && !empty($_GET['id'])  && !empty($_GET['idClient']) && !empty($_GET['dateloc']) && !empty($_GET['datefinloc'])){     
+      ajouterLocation();
     }
     
-
-
-
 // AJOUTER UNE VOITURE
-    if(isset($_GET['action']) && $_GET['action']=="ajouter" && !empty($_GET['marque'])  && !empty($_GET['modele'])  && !empty($_GET['annees']) && !empty($_GET['kilometrage'])){
-        $ajouter = $db->prepare('INSERT INTO vehicules (marque_vehicules, modele_vehicules, annees_vehicules, kilometrage_vehicules ) VALUES (:marque, :modele, :annees, :kilometrage)');
-
-        $ajouter->bindParam(':marque', $_GET['marque'], 
-        PDO::PARAM_STR);
-        $ajouter->bindParam(':modele', $_GET['modele'], 
-        PDO::PARAM_STR);
-        $ajouter->bindParam(':annees', $_GET['annees'], 
-        PDO::PARAM_STR);
-        $ajouter->bindParam(':kilometrage', $_GET['kilometrage'], 
-        PDO::PARAM_STR);
-        $estceok = $ajouter->execute();
-        // $estceok->debugDumpParams();
-            if($estceok){
-                echo 'votre enregistrement a été ajouté avec succés';
-                
-            
-            } else {
-                echo 'Veuillez recommencer svp, une erreur est survenue';
-            }
-        }
-
-
-
+    if(isset($_GET['action']) && $_GET['action']=="ajouter" && !empty($_GET['marque'])  && !empty($_GET['modele'])  && !empty($_GET['annees']) && !empty($_GET['kilometrage'])){     
+      ajouterVoiture();
+    }
 
 // AJOUTER UN CLIENT
-        if(isset($_GET['action']) && $_GET['action']=="ajouter" && !empty($_GET['nom'])  && !empty($_GET['prenom'])  && !empty($_GET['adresse']) && !empty($_GET['cp']) && !empty($_GET['ville'])){
-            $ajouter = $db->prepare('INSERT INTO clients (nom_clients, prenom_clients, adresse_clients, cp_clients, ville_clients) VALUES (:nom, :prenom, :adresse, :cp, :ville)');
-            $ajouter->bindParam(':nom', $_GET['nom'], 
-            PDO::PARAM_STR);
-            $ajouter->bindParam(':prenom', $_GET['prenom'], 
-            PDO::PARAM_STR);
-            $ajouter->bindParam(':adresse', $_GET['adresse'], 
-            PDO::PARAM_STR);
-            $ajouter->bindParam(':cp', $_GET['cp'], 
-            PDO::PARAM_STR);
-            $ajouter->bindParam(':ville', $_GET['ville'], 
-            PDO::PARAM_STR);
-            $estceok = $ajouter->execute();
-            // $estceok->debugDumpParams();
-                if($estceok){
-                    echo 'votre enregistrement a été ajouté avec succés';
-                    
-                
-                } else {
-                    echo 'Veuillez recommencer svp, une erreur est survenue';
-                }
-            }
-
-
-
-
-
-
-
-
+    if(isset($_GET['action']) && $_GET['action']=="ajouter" && !empty($_GET['nom'])  && !empty($_GET['prenom'])  && !empty($_GET['adresse']) && !empty($_GET['cp']) && !empty($_GET['ville'])){      
+      ajouterClient();
+    }
 
 // SUPPRIMER UNE VOITURE
-        if(isset($_GET['action']) && $_GET['action']=="supprimer" && !empty($_GET['id'])){
-        
-            $supprimer = $db->prepare('DELETE FROM vehicules WHERE id_car_vehicules =:id');
-            $supprimer->bindParam(':id', $_GET['id'], 
-            PDO::PARAM_STR);
-            $supprimer = $supprimer->execute();
-                if($supprimer){
-                    echo 'votre enregistrement a bien été supprimé';
-                    
-                
-                } else {
-                    echo 'Veuillez recommencer svp, une erreur est survenue';
-                }
-            }
-// SUPPRIMER UN CLIENT
-if(isset($_GET['action']) && $_GET['action']=="supprimer" && !empty($_GET['id_client'])){
-        
-    $supprimer = $db->prepare('DELETE FROM clients WHERE id_client_clients =:id');
-    $supprimer->bindParam(':id', $_GET['id_client'], 
-    PDO::PARAM_STR);
-    $supprimer = $supprimer->execute();
-        if($supprimer){
-            echo 'votre enregistrement a bien été supprimé';
-            
-        
-        } else {
-            echo 'Veuillez recommencer svp, une erreur est survenue';
-        }
+    if(isset($_GET['action']) && $_GET['action']=="supprimer" && !empty($_GET['id'])){    
+      supprimerVoiture(); 
     }
-
+// SUPPRIMER UN CLIENT
+    if(isset($_GET['action']) && $_GET['action']=="supprimer" && !empty($_GET['id_client'])){   
+      supprimerClient();     
+    }
 
 // SUPPRIMER UNE LOCATION
-if(isset($_GET['action']) && $_GET['action']=="supprimer" && !empty($_GET['id'])){
-        
-    $supprimer = $db->prepare('DELETE FROM louer WHERE id_car_vehicules =:id');
-    $supprimer->bindParam(':id', $_GET['id'], 
-    PDO::PARAM_STR);
-    $supprimer = $supprimer->execute();
-        if($supprimer){
-            echo 'votre enregistrement a bien été supprimé';
-            
-        
-        } else {
-            echo 'Veuillez recommencer svp, une erreur est survenue';
-        }
+    if(isset($_GET['action']) && $_GET['action']=="supprimer" && !empty($_GET['id'])){     
+      supprimerLocation();       
     }
 
-
-
-
-
-
-
-
-
 // MODIFIER UNE VOITURE
-            if(isset($_GET['action']) && $_GET['action']=="modifier"  && !empty($_GET['id'])  && !empty($_GET['marque'])  && !empty($_GET['modele']) && !empty($_GET['annees']) && !empty($_GET['kilometrage'])){
-                $modifier = $db->prepare('UPDATE vehicules SET marque_vehicules = :marque, modele_vehicules = :modele, annees_vehicules = :annees, kilometrage_vehicules = :kilometrage WHERE id_car_vehicules =:id');
-                $modifier->bindParam(':id', $_GET['id'], PDO::PARAM_STR);
-                $modifier->bindParam(':marque', $_GET['marque'], PDO::PARAM_STR);
-                $modifier->bindParam(':modele', $_GET['modele'], PDO::PARAM_STR);
-                $modifier->bindParam(':annees', $_GET['annees'], PDO::PARAM_STR);
-                $modifier->bindParam(':kilometrage', $_GET['kilometrage'], PDO::PARAM_STR);                               
-                $modifier = $modifier->execute();
-                // $modifier->debugDumpParams();
-                // die;
-                    if($modifier){
-                        echo 'votre enregistrement a bien été modifié';
-                        
-                    
-                    } else {
-                        echo 'Veuillez recommencer svp, une erreur est survenue';
-                    }
-                }
+    if(isset($_GET['action']) && $_GET['action']=="modifier"  && !empty($_GET['id'])  && !empty($_GET['marque'])  && !empty($_GET['modele']) && !empty($_GET['annees']) && !empty($_GET['kilometrage'])){
+      modifierVoiture();          
+    }
+
 // MODIFIER UN CLIENT
-            if(isset($_GET['action']) && $_GET['action']=="modifier"  && !empty($_GET['nom'])  && !empty($_GET['prenom'])  && !empty($_GET['adresse']) && !empty($_GET['cp']) && !empty($_GET['ville'])){
-                $modifier = $db->prepare('UPDATE clients SET nom_clients = :nom, prenom_clients = :prenom, adresse_clients = :adresse, cp_clients = :cp, ville_clients = :ville WHERE id_client_clients =:id');
-                $modifier->bindParam(':id', $_GET['id_client'], PDO::PARAM_STR);
-                $modifier->bindParam(':nom', $_GET['nom'], PDO::PARAM_STR);
-                $modifier->bindParam(':prenom', $_GET['prenom'], PDO::PARAM_STR);
-                $modifier->bindParam(':adresse', $_GET['adresse'], PDO::PARAM_STR);
-                $modifier->bindParam(':cp', $_GET['cp'], PDO::PARAM_STR);
-                $modifier->bindParam(':ville', $_GET['ville'], PDO::PARAM_STR);                               
-                $modifier = $modifier->execute();
-                // $modifier->debugDumpParams();
-                // die;
-                    if($modifier){
-                        echo 'votre enregistrement a bien été modifié';
-                        
-                    
-                    } else {
-                        echo 'Veuillez recommencer svp, une erreur est survenue';
-                    }
-                }
+    if(isset($_GET['action']) && $_GET['action']=="modifier"  && !empty($_GET['nom'])  && !empty($_GET['prenom'])  && !empty($_GET['adresse']) && !empty($_GET['cp']) && !empty($_GET['ville'])){
+      modifierClient();         
+    }
 ?>               
-
-
-
-
-
-
-
 
 <!-- 
 AFFICHER LES VOITURES -->
--- function.php
+++ function.php
@@ -1,15 +1,6 @@
 <?php
-  try {
-    $db = new PDO('mysql:host=localhost;dbname=hertz;port=3306', 'root', '');
-    }
-    catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
-}
-?>
-
-<?php
-function ajouterLocation ($) {
+function ajouterLocation () {
+    include 'connect.php';
     $ajouter = $db->prepare('INSERT INTO louer (id_car_vehicules, id_client_clients, date_louer, date_fin) VALUES (:idCar, :idClient, :dateloc, :datefinloc)');
         $ajouter->bindParam(':idCar', $_GET['id'], 
         PDO::PARAM_STR);
@@ -22,13 +13,128 @@ function ajouterLocation ($) {
         $estceok = $ajouter->execute();
         // $estceok->debugDumpParams();
             if($estceok){
-                echo 'votre enregistrement a été ajouté avec succés';
-                
-            
+                echo 'votre enregistrement a été ajouté avec succés';           
             } else {
                 echo 'Veuillez recommencer svp, une erreur est survenue';
             }
+}
 
+function ajouterVoiture () {
+    include 'connect.php';
+    $ajouter = $db->prepare('INSERT INTO vehicules (marque_vehicules, modele_vehicules, annees_vehicules, kilometrage_vehicules ) VALUES (:marque, :modele, :annees, :kilometrage)');
+        $ajouter->bindParam(':marque', $_GET['marque'], 
+        PDO::PARAM_STR);
+        $ajouter->bindParam(':modele', $_GET['modele'], 
+        PDO::PARAM_STR);
+        $ajouter->bindParam(':annees', $_GET['annees'], 
+        PDO::PARAM_STR);
+        $ajouter->bindParam(':kilometrage', $_GET['kilometrage'], 
+        PDO::PARAM_STR);
+        $estceok = $ajouter->execute();
+        // $estceok->debugDumpParams();
+            if($estceok){
+                echo 'votre enregistrement a été ajouté avec succés';           
+            } else {
+                echo 'Veuillez recommencer svp, une erreur est survenue';
+            }
+}
+
+function ajouterClient () {
+    include 'connect.php';
+    $ajouter = $db->prepare('INSERT INTO clients (nom_clients, prenom_clients, adresse_clients, cp_clients, ville_clients) VALUES (:nom, :prenom, :adresse, :cp, :ville)');
+        $ajouter->bindParam(':nom', $_GET['nom'], 
+        PDO::PARAM_STR);
+        $ajouter->bindParam(':prenom', $_GET['prenom'], 
+        PDO::PARAM_STR);
+        $ajouter->bindParam(':adresse', $_GET['adresse'], 
+        PDO::PARAM_STR);
+        $ajouter->bindParam(':cp', $_GET['cp'], 
+        PDO::PARAM_STR);
+        $ajouter->bindParam(':ville', $_GET['ville'], 
+        PDO::PARAM_STR);
+        $estceok = $ajouter->execute();
+        // $estceok->debugDumpParams();
+             if($estceok){
+                    echo 'votre enregistrement a été ajouté avec succés';                
+                } else {
+                    echo 'Veuillez recommencer svp, une erreur est survenue';
+                }
+}
+
+function supprimerVoiture () {
+    include 'connect.php';
+    $supprimer = $db->prepare('DELETE FROM vehicules WHERE id_car_vehicules =:id');
+            $supprimer->bindParam(':id', $_GET['id'], 
+            PDO::PARAM_STR);
+            $supprimer = $supprimer->execute();
+                if($supprimer){
+                    echo 'votre enregistrement a bien été supprimé';               
+                } else {
+                    echo 'Veuillez recommencer svp, une erreur est survenue';
+                }
+}
+
+function supprimerClient () {
+    include 'connect.php';
+    $supprimer = $db->prepare('DELETE FROM clients WHERE id_client_clients =:id');
+    $supprimer->bindParam(':id', $_GET['id_client'], 
+    PDO::PARAM_STR);
+    $supprimer = $supprimer->execute();
+        if($supprimer){
+            echo 'votre enregistrement a bien été supprimé';       
+        } else {
+            echo 'Veuillez recommencer svp, une erreur est survenue';
+        }
+}
+
+function supprimerLocation () {
+    include 'connect.php';
+    $supprimer = $db->prepare('DELETE FROM louer WHERE id_car_vehicules =:id');
+    $supprimer->bindParam(':id', $_GET['id'], 
+    PDO::PARAM_STR);
+    $supprimer = $supprimer->execute();
+        if($supprimer){
+            echo 'votre enregistrement a bien été supprimé';        
+        } else {
+            echo 'Veuillez recommencer svp, une erreur est survenue';
+        }
+}
+
+function modifierVoiture () {
+    include 'connect.php';
+    $modifier = $db->prepare('UPDATE vehicules SET marque_vehicules = :marque, modele_vehicules = :modele, annees_vehicules = :annees, kilometrage_vehicules = :kilometrage WHERE id_car_vehicules =:id');
+                $modifier->bindParam(':id', $_GET['id'], PDO::PARAM_STR);
+                $modifier->bindParam(':marque', $_GET['marque'], PDO::PARAM_STR);
+                $modifier->bindParam(':modele', $_GET['modele'], PDO::PARAM_STR);
+                $modifier->bindParam(':annees', $_GET['annees'], PDO::PARAM_STR);
+                $modifier->bindParam(':kilometrage', $_GET['kilometrage'], PDO::PARAM_STR);                               
+                $modifier = $modifier->execute();
+                // $modifier->debugDumpParams();
+                // die;
+                    if($modifier){
+                        echo 'votre enregistrement a bien été modifié';                   
+                    } else {
+                        echo 'Veuillez recommencer svp, une erreur est survenue';
+                    }
+}
+
+function modifierClient () {
+    include 'connect.php';
+    $modifier = $db->prepare('UPDATE clients SET nom_clients = :nom, prenom_clients = :prenom, adresse_clients = :adresse, cp_clients = :cp, ville_clients = :ville WHERE id_client_clients =:id');
+                $modifier->bindParam(':id', $_GET['id_client'], PDO::PARAM_STR);
+                $modifier->bindParam(':nom', $_GET['nom'], PDO::PARAM_STR);
+                $modifier->bindParam(':prenom', $_GET['prenom'], PDO::PARAM_STR);
+                $modifier->bindParam(':adresse', $_GET['adresse'], PDO::PARAM_STR);
+                $modifier->bindParam(':cp', $_GET['cp'], PDO::PARAM_STR);
+                $modifier->bindParam(':ville', $_GET['ville'], PDO::PARAM_STR);                               
+                $modifier = $modifier->execute();
+                // $modifier->debugDumpParams();
+                // die;
+                    if($modifier){
+                        echo 'votre enregistrement a bien été modifié';                  
+                    } else {
+                        echo 'Veuillez recommencer svp, une erreur est survenue';
+                    }
 }
 
 ?>
