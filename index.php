640ef44e09e969bb708af21f147a057315959972
loicdauchy <l.dauchy@codeur.online>
2020-11-06 13:57:21 +0100

    adding js file

--- index.php
+++ index.php
@@ -44,6 +44,7 @@ $lister = $db->prepare('SELECT * FROM louer
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
     <link rel="shortcut icon" href="image/icone.png" type="image/x-icon"/> 
     <title>hertz</title>
 </head>
@@ -89,7 +90,13 @@ $lister = $db->prepare('SELECT * FROM louer
     <section id="vehiculesList container-fluid" class="listevoituremain">
         <div class="titretables">
             <hr>
-            <center><h2>Liste véhicule</h2></center>
+            <center>
+            <h2><span
+             class="txt-rotate"
+             data-period="1000"
+             data-rotate='[ "Miste des véhicules", "Liste des véhicules"]'>
+             </span></h2>
+             </center>
             <hr>
         </div>
         
