160747330ecc91b4bd3bdcee1cda446b0bcb1ab7
Triv-johan <guyon.johan@laposte.net>
2020-12-16 09:04:51 +0100

    base de donnée en ligne

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
   try {
-    $db = new PDO('mysql:host=localhost;dbname=hertz;port=3306', 'root', '');
+    $db = new PDO('mysql:host=localhost;dbname=hertz;port=3306', 'johang', '4E3r+JZ43Q7Kgw==');
     }
     catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
